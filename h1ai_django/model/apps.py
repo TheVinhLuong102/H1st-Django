d288a1a762383f1c12d8aeb05942f05915e09831
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-11-23 16:03:30 -0800

    refactor h1ai_django.model

--- h1ai_django/model/apps.py
+++ h1ai_django/model/apps.py
@@ -1,13 +1,13 @@
 from django.apps.config import AppConfig
 
 
-class H1AIAppConfig(AppConfig):
+class H1AIModelAppConfig(AppConfig):
     # AppConfig.name
     # Full Python path to the application, e.g. 'django.contrib.admin'.
     # This attribute defines which application the configuration applies to.
     # It must be set in all AppConfig subclasses.
     # It must be unique across a Django project.
-    name = 'h1ai_django.ai'
+    name = 'h1ai_django.model'
 
     # AppConfig.label
     # Short name for the application, e.g. 'admin'
@@ -16,9 +16,9 @@ class H1AIAppConfig(AppConfig):
     # It defaults to the last component of name.
     # It should be a valid Python identifier.
     # It must be unique across a Django project.
-    label = 'H1AI'
+    label = 'H1AI_Model'
 
     # AppConfig.verbose_name
     # Human-readable name for the application, e.g. “Administration”.
     # This attribute defaults to label.title().
-    verbose_name = 'Human-First AI'
+    verbose_name = 'Human-First AI: Model Administration'
