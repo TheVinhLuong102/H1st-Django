2e5911d89bc9cf1a3eaea9cff955ce850ca402f5
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-11-29 01:25:52 -0800

    add h1st_django.data.models.Dataset.Meta options

--- h1st_django/data/models.py
+++ h1st_django/data/models.py
@@ -4,6 +4,9 @@ from polymorphic.models import PolymorphicModel
 
 from uuid import uuid4
 
+from ..util import PGSQL_IDENTIFIER_MAX_LEN
+from .apps import H1stDataAppConfig
+
 
 class Dataset(PolymorphicModel):
     uuid = \
@@ -15,6 +18,17 @@ class Dataset(PolymorphicModel):
             unique=True,   # implied
             db_index=True,
             editable=False)
+
+    class Meta:
+        db_table = f"{H1stDataAppConfig.label}_{__qualname__.split('.')[0]}"
+
+        assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
+            ValueError(f'*** "{db_table}" TOO LONG ***')
+
+        default_related_name = 'datasets'
+
+        verbose_name = 'Dataset'
+        verbose_name_plural = 'Datasets'
 
     def __str__(self):
         return f'{type(self)} #{self.uuid}'
