48452084cc8b9d786f9d575431026b60f086d0fe
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-11-28 18:31:50 -0800

    add basic packaging files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import json
+from setuptools import find_namespace_packages, setup
+
+
+_METADATA_FILE_NAME = 'metadata.json'
+_PACKAGE_NAMESPACE_NAME = 'h1ai_django'
+_REQUIREMENTS_FILE_NAME = 'requirements.txt'
+
+
+_metadata = json.load(open(_METADATA_FILE_NAME, 'r'))
+
+
+setup(
+    name=_metadata['PACKAGE'],
+    version=_metadata['VERSION'],
+    namespace_packages=[_PACKAGE_NAMESPACE_NAME],
+    packages=find_namespace_packages(include=[f'{_PACKAGE_NAMESPACE_NAME}.*']),
+    include_package_data=True,
+    install_requires=[s
+                      for s in {i.strip() for i in
+                                open(_REQUIREMENTS_FILE_NAME).readlines()}
+                      if not s.startswith('#')])
