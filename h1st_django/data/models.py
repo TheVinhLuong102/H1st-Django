1bac3a5a8bb7eda807bdd461ad5b0fdd0b86c6b8
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-11-29 00:21:16 -0800

    add h1st_django.data.models.Dataset

---
+++ h1st_django/data/models.py
@@ -0,0 +1,20 @@
+from django.db.models.fields import UUIDField
+
+from polymorphic.models import PolymorphicModel
+
+from uuid import uuid4
+
+
+class Dataset(PolymorphicModel):
+    uuid = \
+        UUIDField(
+            verbose_name='UUID',
+            default=uuid4,
+            primary_key=True,
+            null=False,   # implied
+            unique=True,   # implied
+            db_index=True,
+            editable=False)
+
+    def __str__(self):
+        return f'{type(self)} #{self.uuid}'
