cba1f4191d9be0d79efe2537098d435e41053dd3
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-04 12:27:55 -0800

    add h1st.django.util.cli._standard_files

--- h1st/django/util/cli/__init__.py
+++ h1st/django/util/cli/__init__.py
@@ -4,7 +4,22 @@ from ruamel import yaml
 import shutil
 
 
-_H1ST_DJANGO_CONFIG_FILE_NAME = _H1ST_DJANGO_CONFIG_FILE_PATH = '.config.yml'
+_H1ST_DJANGO_UTIL_CLI_STANDARD_FILES_DIR_PATH = \
+    Path(__file__).parent / '_standard_files'
+
+_H1ST_DJANGO_CONFIG_FILE_NAME = '.config.yml'
+
+_MANAGE_PY_FILE_NAME = 'manage.py'
+_MANAGE_PY_FILE_SRC_PATH = \
+    _H1ST_DJANGO_UTIL_CLI_STANDARD_FILES_DIR_PATH / _MANAGE_PY_FILE_NAME
+
+_ASGI_PY_FILE_NAME = 'asgi.py'
+_ASGI_PY_FILE_SRC_PATH = \
+    _H1ST_DJANGO_UTIL_CLI_STANDARD_FILES_DIR_PATH / _ASGI_PY_FILE_NAME
+
+_WSGI_PY_FILE_NAME = 'wsgi.py'
+_WSGI_PY_FILE_SRC_PATH = \
+    _H1ST_DJANGO_UTIL_CLI_STANDARD_FILES_DIR_PATH / _WSGI_PY_FILE_NAME
 
 
 def run_command_with_config_file(
@@ -21,12 +36,30 @@ def run_command_with_config_file(
        and db_creds['password'] \
        and db_creds['db-name']
 
-    assert not os.path.exists(path=_H1ST_DJANGO_CONFIG_FILE_PATH)
+    assert not os.path.exists(path=_H1ST_DJANGO_CONFIG_FILE_NAME)
     shutil.copyfile(
         src=h1st_django_config_file_path,
-        dst=_H1ST_DJANGO_CONFIG_FILE_PATH)
+        dst=_H1ST_DJANGO_CONFIG_FILE_NAME)
+    assert not os.path.exists(path=_MANAGE_PY_FILE_NAME)
+    shutil.copyfile(
+        src=_MANAGE_PY_FILE_SRC_PATH,
+        dst=_MANAGE_PY_FILE_NAME)
+    assert not os.path.exists(path=_ASGI_PY_FILE_NAME)
+    shutil.copyfile(
+        src=_ASGI_PY_FILE_SRC_PATH,
+        dst=_ASGI_PY_FILE_NAME)
+    assert not os.path.exists(path=_WSGI_PY_FILE_NAME)
+    shutil.copyfile(
+        src=_WSGI_PY_FILE_SRC_PATH,
+        dst=_WSGI_PY_FILE_NAME)
 
     os.system(command)
 
-    os.remove(_H1ST_DJANGO_CONFIG_FILE_PATH)
-    assert not os.path.exists(path=_H1ST_DJANGO_CONFIG_FILE_PATH)
+    os.remove(_H1ST_DJANGO_CONFIG_FILE_NAME)
+    assert not os.path.exists(path=_H1ST_DJANGO_CONFIG_FILE_NAME)
+    os.remove(_MANAGE_PY_FILE_NAME)
+    assert not os.path.exists(path=_MANAGE_PY_FILE_NAME)
+    os.remove(_ASGI_PY_FILE_NAME)
+    assert not os.path.exists(path=_ASGI_PY_FILE_NAME)
+    os.remove(_WSGI_PY_FILE_NAME)
+    assert not os.path.exists(path=_WSGI_PY_FILE_NAME)
