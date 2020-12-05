f90460ad30b5cc270077832ab1ba0984ce4a4276
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-05 03:28:02 -0800

    enhance h1st.django.model.views with sync_to_async

--- h1st/django/model/views.py
+++ h1st/django/model/views.py
@@ -1,3 +1,4 @@
+from asgiref.sync import sync_to_async
 from django.http.response import JsonResponse
 
 import json
@@ -6,5 +7,9 @@ from .models import H1stDjangoModel
 
 
 async def h1st_model_predict(request, h1st_model_uuid, input_data):
-    h1st_model = H1stDjangoModel.objects.get(uuid=h1st_model_uuid)
+    h1st_model = await \
+        sync_to_async(
+            H1stDjangoModel.objects.get,
+            thread_sensitive=False)(uuid=h1st_model_uuid)
+
     return JsonResponse(h1st_model.predict(json.loads(input_data)))
