2046f1c158bbd624c7bb55b779ae65df47c61215
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-04 20:06:45 -0800

    Revert "try ASGI server"
    This reverts commit 5a957fe05b92cd09a2cdbf1ced0384d138c36d00.

--- h1st/django/model/views.py
+++ h1st/django/model/views.py
@@ -5,6 +5,6 @@ import json
 from .models import H1stDjangoModel
 
 
-async def h1st_model_predict(request, h1st_model_uuid, input_data):
+def h1st_model_predict(request, h1st_model_uuid, input_data):
     h1st_model = H1stDjangoModel.objects.get(uuid=h1st_model_uuid)
     return JsonResponse(h1st_model.predict(json.loads(input_data)))
