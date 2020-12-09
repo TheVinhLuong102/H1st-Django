16276a31824d4d30cd2bce594734c95cc254326a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-08 17:45:33 -0800

    fix h1st.django.model.views.h1st_model_call(...)

--- h1st/django/model/views.py
+++ h1st/django/model/views.py
@@ -6,17 +6,18 @@ from ..data.models import JSONDataSet
 from .models import H1stDjangoModel, Decision
 
 
-def h1st_model_predict(request, h1st_model_uuid, input_data):
-    json_data_set = JSONDataSet.objects.create(json=input_data)
+def h1st_model_call(request, h1st_model_uuid, input_data):
+    input_json = json.loads(input_data)
+
+    json_data_set = JSONDataSet.objects.create(json=input_json)
 
     h1st_model = H1stDjangoModel.objects.get(uuid=h1st_model_uuid)
 
-    decision_output = h1st_model.predict(json.loads(input_data))
+    decision_output_json = h1st_model(input_json)
 
-    decision = \
-        Decision.objects.create(
-            data_set=json_data_set,
-            h1st_model=h1st_model,
-            output_json=decision_output)
+    Decision.objects.create(
+        data_set=json_data_set,
+        h1st_model=h1st_model,
+        output_json=decision_output_json)
 
-    return JsonResponse(decision.output_json)
+    return JsonResponse(decision_output_json)
