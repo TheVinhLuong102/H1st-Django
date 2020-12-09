d27a14d24e6f574def343e8ebdb026a78885a7ec
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-08 17:37:20 -0800

    add h1st.django.model.admin.DecisionAdmin

--- h1st/django/model/admin.py
+++ h1st/django/model/admin.py
@@ -2,13 +2,20 @@ from django.contrib.admin.decorators import register
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.sites import site
 
-from .models import H1stDjangoModel, H1stModelEvalMetricsSet
+from .models import H1stDjangoModel, Decision, H1stModelEvalMetricsSet
 
 
 @register(
     H1stDjangoModel,
     site=site)
 class H1stModelAdmin(ModelAdmin):
+    pass
+
+
+@register(
+    Decision,
+    site=site)
+class DecisionAdmin(ModelAdmin):
     pass
 
 
