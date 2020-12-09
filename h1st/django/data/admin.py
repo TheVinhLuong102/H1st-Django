4c6f577de69c7063d90f97cd26680797da920767
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-09 00:32:19 -0800

    remove h1st.django.data.admin.DataSetAdmin

--- h1st/django/data/admin.py
+++ h1st/django/data/admin.py
@@ -2,20 +2,13 @@ from django.contrib.admin.decorators import register
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.sites import site
 
-from .models import DataSchema, DataSet, JSONDataSet, ParquetDataSet
+from .models import DataSchema, JSONDataSet, ParquetDataSet
 
 
 @register(
     DataSchema,
     site=site)
 class DataSchemaAdmin(ModelAdmin):
-    pass
-
-
-@register(
-    DataSet,
-    site=site)
-class DataSetAdmin(ModelAdmin):
     pass
 
 
