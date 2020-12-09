ef93ff9953f0c8639d0d3057774cb98b18eef9d3
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-09 12:23:00 -0800

    add h1st.django.trust.models.RecordedModelRun, RecordedModelFittingRun, RecordedModelInferenceRun

--- h1st/django/trust/admin.py
+++ h1st/django/trust/admin.py
@@ -2,3 +2,18 @@ from django.contrib.admin.decorators import register
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.sites import site
 
+from .models import RecordedModelFittingRun, RecordedModelInferenceRun
+
+
+@register(
+    RecordedModelFittingRun,
+    site=site)
+class RecordedModelFittingRunAdmin(ModelAdmin):
+    pass
+
+
+@register(
+    RecordedModelInferenceRun,
+    site=site)
+class RecordedModelInferenceRunAdmin(ModelAdmin):
+    pass
--- h1st/django/trust/models.py
+++ h1st/django/trust/models.py
@@ -5,6 +5,8 @@ from django.db.models.fields import UUIDField
 from django.db.models.fields.json import JSONField
 from django.db.models.fields.related import ForeignKey
 
+from polymorphic.models import PolymorphicModel
+
 from json.decoder import JSONDecoder
 from uuid import uuid4
 
@@ -14,3 +16,56 @@ from ..util import PGSQL_IDENTIFIER_MAX_LEN
 from . import H1stTrustAppConfig
 
 
+class RecordedModelRun(PolymorphicModel):
+    uuid = \
+        UUIDField(
+            verbose_name='UUID',
+            default=uuid4,
+            primary_key=True,
+            null=False,   # implied
+            unique=True,   # implied
+            db_index=True,
+            editable=False)
+
+    class Meta:
+        db_table = f"{H1stTrustAppConfig.label}_{__qualname__.split('.')[0]}"
+        assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
+            ValueError(f'*** "{db_table}" TOO LONG ***')
+
+        default_related_name = 'recorded_model_runs'
+
+        verbose_name = 'Recorded Model Run'
+        verbose_name_plural = 'Recorded Model Runs'
+
+    def __str__(self):
+        return f'{type(self).__name__} #{self.uuid}'
+
+
+class RecordedModelFittingRun(RecordedModelRun):
+    class Meta:
+        db_table = f"{H1stTrustAppConfig.label}_{__qualname__.split('.')[0]}"
+        assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
+            ValueError(f'*** "{db_table}" TOO LONG ***')
+
+        default_related_name = 'recorded_model_fitting_runs'
+
+        verbose_name = 'Recorded Model Fitting Run'
+        verbose_name_plural = 'Recorded Model Fitting Runs'
+
+    def __str__(self):
+        return f'{type(self).__name__} #{self.uuid}'
+
+
+class RecordedModelInferenceRun(RecordedModelRun):
+    class Meta:
+        db_table = f"{H1stTrustAppConfig.label}_{__qualname__.split('.')[0]}"
+        assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
+            ValueError(f'*** "{db_table}" TOO LONG ***')
+
+        default_related_name = 'recorded_model_inference_runs'
+
+        verbose_name = 'Recorded Model Inference Run'
+        verbose_name_plural = 'Recorded Model Inference Runs'
+
+    def __str__(self):
+        return f'{type(self).__name__} #{self.uuid}'
