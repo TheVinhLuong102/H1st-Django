08e4d1964f48f6c5f1f3e779b395ff3eda3341e1
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-10 17:44:35 -0800

    enhance h1st.django.util.pip.get_python_dependencies()

--- h1st/django/util/pip.py
+++ h1st/django/util/pip.py
@@ -2,4 +2,16 @@ from pip._internal.operations.freeze import freeze
 
 
 def get_python_dependencies():
-    return list(freeze())
+    d = {}
+
+    for deps_and_vers in freeze():
+        ls = deps_and_vers.split('==')
+
+        if len(ls) == 2:
+            d[ls[0]] = ls[1]
+
+        else:
+            assert len(ls) == 1, f'*** {ls} ***'
+            d[ls[0]] = None
+
+    return d
