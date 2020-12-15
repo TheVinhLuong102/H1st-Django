c545370d7b0911e937f13f7093bcf3ea9af2e1aa
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-15 10:47:54 -0800

    refactor h1st.django.data

--- h1st/django/data/admin.py
+++ h1st/django/data/admin.py
@@ -9,7 +9,31 @@ from .models import DataSchema, JSONDataSet, ParquetDataSet, TFRecordDataSet
 
 @register(DataSchema, site=site)
 class DataSchemaAdmin(ModelAdmin):
+    list_display = \
+        'name', \
+        'specs', \
+        'modified'
+
+    list_display_links = \
+        'name', \
+        'specs'
+
+    search_fields = \
+        'name', \
+        'specs'
+
     show_full_result_count = False
+
+    readonly_fields = \
+        'created', \
+        'modified'
+
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request=request)
+
+        return query_set \
+            if request.resolver_match.url_name.endswith('_change') \
+          else query_set.only(*self.list_display)
 
     @silk_profile(name=f'{__module__}: {DataSchema._meta.verbose_name}')
     def changeform_view(self, *args, **kwargs):
@@ -22,7 +46,27 @@ class DataSchemaAdmin(ModelAdmin):
 
 @register(JSONDataSet, site=site)
 class JSONDataSetAdmin(ModelAdmin):
+    list_display = \
+        'name', \
+        'schema__name', \
+        'modified'
+
+    list_display_links = 'name',
+
+    search_fields = 'name',
+
     show_full_result_count = False
+
+    readonly_fields = \
+        'created', \
+        'modified'
+
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request=request)
+
+        return query_set \
+            if request.resolver_match.url_name.endswith('_change') \
+          else query_set.only(*self.list_display)
 
     @silk_profile(name=f'{__module__}: {JSONDataSet._meta.verbose_name}')
     def changeform_view(self, *args, **kwargs):
@@ -36,7 +80,27 @@ class JSONDataSetAdmin(ModelAdmin):
 
 @register(ParquetDataSet, site=site)
 class ParquetDataSetAdmin(ModelAdmin):
+    list_display = \
+        'name', \
+        'schema__name', \
+        'modified'
+
+    list_display_links = 'name',
+
+    search_fields = 'name',
+
     show_full_result_count = False
+
+    readonly_fields = \
+        'created', \
+        'modified'
+
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request=request)
+
+        return query_set \
+            if request.resolver_match.url_name.endswith('_change') \
+          else query_set.only(*self.list_display)
 
     @silk_profile(name=f'{__module__}: {ParquetDataSet._meta.verbose_name}')
     def changeform_view(self, *args, **kwargs):
