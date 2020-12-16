3dda23afb392abf57801cc8887050daf66cf520d
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-15 17:25:07 -0800

    refactor h1st.django.data.models.DataSet

--- h1st/django/data/models.py
+++ h1st/django/data/models.py
@@ -1,8 +1,8 @@
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models.deletion import PROTECT
+from django.db.models.deletion import CASCADE, PROTECT, SET_NULL
 from django.db.models.fields import CharField
 from django.db.models.fields.json import JSONField
-from django.db.models.fields.related import ForeignKey
+from django.db.models.fields.related import ForeignKey, OneToOneField
 
 from polymorphic.models import PolymorphicModel
 
@@ -18,26 +18,48 @@ class DataSchema(PolymorphicModel, DjangoModelWithUUIDPKAndTimestamps):
         CharField(
             verbose_name='Data Schema Unique Name',
             help_text='Data Schema Unique Name',
+
             max_length=255,
+
             null=False,
             blank=False,
+            choices=None,
+            db_column=None,
             db_index=True,
+            db_tablespace=None,
             default=None,
             editable=True,
-            unique=True)
+            # error_messages=None,
+            primary_key=False,
+            unique=True,
+            unique_for_date=None, unique_for_month=None, unique_for_year=None,
+            # validators=None
+        )
 
     specs = \
         JSONField(
             verbose_name='Data Schema Specifications',
             help_text='Data Schema Specifications',
+
             encoder=DjangoJSONEncoder,
             decoder=JSONDecoder,
+
             null=False,
             blank=False,
+            choices=None,
+            db_column=None,
+            db_index=False,
+            db_tablespace=None,
             default=None,
-            editable=True)
+            editable=True,
+            # error_messages=None,
+            primary_key=False,
+            unique=False,
+            unique_for_date=None, unique_for_month=None, unique_for_year=None,
+            # validators=None
+        )
 
-    class Meta:
+    class Meta(DjangoModelWithUUIDPKAndTimestamps.Meta):
         verbose_name = 'Data Schema'
         verbose_name_plural = 'Data Schemas'
 
@@ -57,33 +79,32 @@ class DataSet(PolymorphicModel, DjangoModelWithUUIDPKAndTimestamps):
     RELATED_NAME = 'data_sets'
     RELATED_QUERY_NAME = 'data_set'
 
-    name = \
-        CharField(
-            verbose_name='Data Set Unique Name',
-            help_text='Data Set Unique Name',
-            max_length=255,
-            null=False,
-            blank=False,
-            db_index=True,
-            default=None,
-            editable=True,
-            unique=True)
-
     schema = \
         ForeignKey(
             verbose_name='Data Set Schema',
             help_text='Data Set Schema',
+
             to=DataSchema,
-            on_delete=PROTECT,
+            on_delete=SET_NULL,
             related_name=RELATED_NAME,
             related_query_name=RELATED_QUERY_NAME,
+
             null=True,
             blank=True,
+            choices=None,
+            db_column=None,
             db_index=True,   # implied
+            db_tablespace=None,
             default=None,
-            editable=True)
+            editable=True,
+            # error_messages=None,
+            primary_key=False,
+            unique=False,
+            unique_for_date=None, unique_for_month=None, unique_for_year=None,
+            # validators=None
+        )
 
-    class Meta:
+    class Meta(DjangoModelWithUUIDPKAndTimestamps.Meta):
         verbose_name = 'Data Set'
         verbose_name_plural = 'Data Sets'
 
@@ -92,6 +113,10 @@ class DataSet(PolymorphicModel, DjangoModelWithUUIDPKAndTimestamps):
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
         default_related_name = 'data_sets'
+
+    def __str__(self) -> str:
+        return f'{type(self).__name__} #{self.uuid}'
+
 
         ordering = 'name',
 
