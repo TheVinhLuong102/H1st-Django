c6a333a6eca8e8398f943035227552982ead993d
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-17 11:53:05 -0800

    add SQLite3 to supported DB engines

--- h1st/django/util/config.py
+++ h1st/django/util/config.py
@@ -15,28 +15,32 @@ def parse_config_file(path=_H1ST_DJANGO_CONFIG_FILE_NAME):
         db_config = config.get('db')
         assert db_config, f'*** "db" KEY NOT FOUND IN {config} ***'
 
-        db_host = db_config.get('host')
-        assert db_host, f'*** "host" KEY NOT FOUND IN {db_config} ***'
-
         db_engine = db_config.get('engine')
         assert db_engine, f'*** "engine" KEY NOT FOUND IN {db_config} ***'
-        assert db_engine in ('postgresql', 'mysql'), \
+        assert db_engine in ('postgresql', 'mysql', 'sqlite3'), \
             ValueError(f'*** "{db_engine}" DATABASE ENGINE NOT SUPPORTED ***')
-
-        db_user = db_config.get('user')
-        assert db_user, f'*** "user" KEY NOT FOUND IN {db_config} ***'
-        db_password = db_config.get('password')
-        assert db_password, f'*** "password" KEY NOT FOUND IN {db_config} ***'
 
         db_name = db_config.get('name')
         assert db_name, f'*** "name" KEY NOT FOUND IN {db_config} ***'
 
-        config['db'] = dict(HOST=db_host,
-                            ENGINE=f'django.db.backends.{db_engine}',
-                            PORT=5432 if db_engine == 'postgresql' else 3306,
+        db_host = db_config.get('host')
+        db_user = db_config.get('user')
+        db_password = db_config.get('password')
+        if db_engine != 'sqlite3':
+            assert db_host, f'*** HOST NOT FOUND IN {db_config} ***'
+            assert db_user, f'*** USER NOT FOUND IN {db_config} ***'
+            assert db_password, f'*** PASSWORD NOT FOUND IN {db_config} ***'
+
+        config['db'] = dict(ENGINE=f'django.db.backends.{db_engine}',
+                            NAME=db_name,
+                            HOST=db_host,
+                            PORT=(5432
+                                  if db_engine == 'postgresql'
+                                  else (3306
+                                        if db_engine == 'mysql'
+                                        else None)),
                             USER=db_user,
-                            PASSWORD=db_password,
-                            NAME=db_name)
+                            PASSWORD=db_password)
 
         return config
 
