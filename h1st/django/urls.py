3f5d8664c8e0b5e5c058fd583dadc96ccad40eb8
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-23 02:47:49 -0800

    Revert "update h1st.django.urls"
    This reverts commit 9d093000c821fe323459e2ec03a99ffe726a5f1d.

--- h1st/django/urls.py
+++ h1st/django/urls.py
@@ -1,5 +1,4 @@
 from django.urls.conf import include, path
-from django.views.generic.base import RedirectView
 
 from .data import urls as data_urls
 from .model import urls as model_urls
@@ -7,7 +6,6 @@ from .trust import urls as trust_urls
 
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='admin')),
     path('data/', include(data_urls)),
     path('models/', include(model_urls)),
     path('trust/', include(trust_urls))
