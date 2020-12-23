9d093000c821fe323459e2ec03a99ffe726a5f1d
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-23 02:26:40 -0800

    update h1st.django.urls

--- h1st/django/urls.py
+++ h1st/django/urls.py
@@ -1,4 +1,5 @@
 from django.urls.conf import include, path
+from django.views.generic.base import RedirectView
 
 from .data import urls as data_urls
 from .model import urls as model_urls
@@ -6,6 +7,7 @@ from .trust import urls as trust_urls
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin')),
     path('data/', include(data_urls)),
     path('models/', include(model_urls)),
     path('trust/', include(trust_urls))
