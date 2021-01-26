579e43eee5bec4cc3b311d85384d7ad97838cf79
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-01-26 12:35:04 -0800

    add h1st.django.model.models.Graph

--- h1st/django/model/models.py
+++ h1st/django/model/models.py
@@ -1,3 +1,4 @@
+from h1st.core.graph import Graph as _CoreH1stGraph
 from h1st.core.model import Model as _CoreH1stModel
 
 from polymorphic.models import PolymorphicModel
@@ -28,3 +29,26 @@ class Model(PolymorphicModel, _ModelWithUUIDPKAndTimestamps, _CoreH1stModel):
 
 # alias
 H1stModel = Model
+
+
+class Graph(PolymorphicModel, _ModelWithUUIDPKAndTimestamps, _CoreH1stGraph):
+    class Meta(_ModelWithUUIDPKAndTimestamps.Meta):
+        verbose_name = 'H1st Graph'
+        verbose_name_plural = 'H1st Graphs'
+
+        db_table = \
+            f"{H1stModelModuleConfig.label}_{__qualname__.split('.')[0]}"
+        assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
+            ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
+
+        default_related_name = 'h1st_graphs'
+
+    def __str__(self) -> str:
+        return f'{type(self).__name__} #{self.uuid}'
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+# alias
+H1stGraph = Graph
