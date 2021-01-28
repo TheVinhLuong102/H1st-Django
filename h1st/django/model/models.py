7faf8ee30795415726f7d66d06db474927f30791
Nhan Vu <nhanitvn@gmail.com>
2021-01-28 03:14:48 +0000

    rename h1st.django.model.models.Workflow

--- h1st/django/model/models.py
+++ h1st/django/model/models.py
@@ -1,4 +1,4 @@
-from h1st.core.graph import Graph as _CoreH1stGraph
+from h1st.core.graph import Graph as _CoreH1stWorkflow
 from h1st.core.model import Model as _CoreH1stModel
 
 from polymorphic.models import PolymorphicModel
@@ -31,17 +31,17 @@ class Model(PolymorphicModel, _ModelWithUUIDPKAndTimestamps, _CoreH1stModel):
 H1stModel = Model
 
 
-class Graph(Model, _CoreH1stGraph):
+class Workflow(Model, _CoreH1stWorkflow):
     class Meta(_ModelWithUUIDPKAndTimestamps.Meta):
-        verbose_name = 'H1st Graph'
-        verbose_name_plural = 'H1st Graphs'
+        verbose_name = 'H1st Workflow'
+        verbose_name_plural = 'H1st Workflows'
 
         db_table = \
             f"{H1stModelModuleConfig.label}_{__qualname__.split('.')[0]}"
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'h1st_graphs'
+        default_related_name = 'h1st_workflows'
 
     def __str__(self) -> str:
         return f'{type(self).__name__} #{self.uuid}'
@@ -50,5 +50,5 @@ class Graph(Model, _CoreH1stGraph):
         raise NotImplementedError
 
 
-# alias
-H1stGraph = Graph
+# aliases
+H1stGraph = Graph = H1stWorkflow = Workflow
