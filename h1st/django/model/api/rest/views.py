29b968746cf83c28d32517034e72326ba45c350b
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-01-28 13:17:27 -0800

    Revert "update h1st.django.model.api.rest.views"
    This reverts commit 4c44a3611692b244b0e92853a9e93b27dab46533.

--- h1st/django/model/api/rest/views.py
+++ h1st/django/model/api/rest/views.py
@@ -58,9 +58,6 @@ class H1stModelViewSet(ModelViewSet):
         CoreJSONRenderer, \
         JSONRenderer
 
-    def get_permissions(self):
-        return (IsAuthenticated(),)
-
     @silk_profile(name=f'{__module__}: {Model._meta.verbose_name_plural}')
     def list(self, *args, **kwargs):
         return super().list(*args, **kwargs)
