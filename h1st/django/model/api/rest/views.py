a769520681f3072b5abf7e77128a10919077881a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-01-28 15:48:15 -0800

    update h1st.django.model.api.rest.views.ModelCallAPIView

--- h1st/django/model/api/rest/views.py
+++ h1st/django/model/api/rest/views.py
@@ -84,6 +84,37 @@ class ModelCallAPIView(APIView):
 
     def post(self, request, *args, **kwargs):
         return Response(dict(
-            content_type=request.content_type,
-            request_keys=dir(request)
-        ))
+
+                request_parsing=dict(
+                    data=str(request.data),
+                    DATA=str(request.DATA),
+                    FILES=str(request.FILES),
+                    POST=str(request.POST),
+
+                    query_params=str(request.query_params),
+                    QUERY_PARAMS=str(request.QUERY_PARAMS),
+
+                    parsers=str(request.parsers)),
+
+                content_negotiation=dict(
+                    accepted_renderer=str(request.accepted_renderer),
+                    accepted_media_type=str(request.accepted_media_type)),
+
+                authentication=dict(
+                    user=str(request.user),
+                    auth=str(request.auth),
+                    authenticators=str(request.authenticators)),
+
+                browser_enhancements=dict(
+                    method=str(request.method),
+                    content_type=str(request.content_type),
+                    stream=str(request.stream)),
+
+                other=dict(
+                    force_plaintext_errors=str(request.force_plaintext_errors),
+                    negotiator=str(request.negotiator),
+                    parser_context=str(request.parser_context),
+                    succ_authenticator=str(request.successful_authenticator),
+                    version=str(request.version),
+                    versioning_scheme=str(request.versioning_scheme))
+                ))
