7de6bdf003e8a78454644fee8972e1f6dbbd8fdd
TheVinhLuong102 <Vinh@Arimo.com>
2021-01-28 07:38:27 +0000

    fix method resolution of h1st.django.model.models.Workflow

--- h1st/django/model/migrations/0002_workflow.py
+++ h1st/django/model/migrations/0002_workflow.py
@@ -44,6 +44,6 @@ class Migration(migrations.Migration):
                 'default_manager_name': 'objects'
             },
 
-            bases=('H1stModel.model',
-                   h1st.core.graph.Graph)
+            bases=(h1st.core.graph.Graph,
+                   'H1stModel.model')
         ),
