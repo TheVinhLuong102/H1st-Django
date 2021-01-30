587771229540a1a86efe76361aea0e15d8dff534
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-01-29 18:03:02 -0800

    update h1st.django.data.admin

--- h1st/django/data/admin.py
+++ h1st/django/data/admin.py
@@ -7,7 +7,11 @@ from silk.profiling.profiler import silk_profile
 from .models import \
     DataSchema, \
     JSONDataSet, NamedJSONDataSet, \
-    NamedCSVDataSet, NamedParquetDataSet, NamedTFRecordDataSet
+    NumPyArray, NamedNumPyArray, \
+    PandasDataFrame, NamedPandasDataFrame, \
+    CSVDataSet, NamedCSVDataSet, \
+    ParquetDataSet, NamedParquetDataSet, \
+    TFRecordDataSet, NamedTFRecordDataSet
 
 
 @register(DataSchema, site=site)
@@ -51,6 +55,78 @@ class NamedJSONDataSetAdmin(ModelAdmin):
         return super().changelist_view(*args, **kwargs)
 
 
+@register(NumPyArray, site=site)
+class NumPyArrayAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(name=f'{__module__}: {NumPyArray._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {NumPyArray._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
+@register(NamedNumPyArray, site=site)
+class NamedNumPyArrayAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(name=f'{__module__}: {NamedNumPyArray._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {NamedNumPyArray._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
+@register(PandasDataFrame, site=site)
+class PandasDataFrameAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(name=f'{__module__}: {PandasDataFrame._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {PandasDataFrame._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
+@register(NamedPandasDataFrame, site=site)
+class NamedPandasDataFrameAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(
+        name=f'{__module__}: {NamedPandasDataFrame._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {NamedPandasDataFrame._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
+@register(CSVDataSet, site=site)
+class CSVDataSetAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(
+        name=f'{__module__}: {CSVDataSet._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {CSVDataSet._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
 @register(NamedCSVDataSet, site=site)
 class NamedCSVDataSetAdmin(ModelAdmin):
     show_full_result_count = False
@@ -66,6 +142,21 @@ class NamedCSVDataSetAdmin(ModelAdmin):
         return super().changelist_view(*args, **kwargs)
 
 
+@register(ParquetDataSet, site=site)
+class ParquetDataSetAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(
+        name=f'{__module__}: {ParquetDataSet._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {ParquetDataSet._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
 @register(NamedParquetDataSet, site=site)
 class NamedParquetDataSetAdmin(ModelAdmin):
     show_full_result_count = False
@@ -77,6 +168,21 @@ class NamedParquetDataSetAdmin(ModelAdmin):
 
     @silk_profile(
         name=f'{__module__}: {NamedParquetDataSet._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
+@register(TFRecordDataSet, site=site)
+class TFRecordDataSetAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(
+        name=f'{__module__}: {TFRecordDataSet._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=f'{__module__}: {TFRecordDataSet._meta.verbose_name_plural}')
     def changelist_view(self, *args, **kwargs):
         return super().changelist_view(*args, **kwargs)
 
