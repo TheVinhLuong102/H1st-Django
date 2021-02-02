6926c2fd48a5721917f764d67c5e129723f9bd3f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-02-02 02:57:31 -0800

    Revert "minor fix to h1st.django.util.config.config_app(...)"
    This reverts commit e79b831e8589e52428400d51deab970d5251af54.

--- h1st/django/util/config.py
+++ h1st/django/util/config.py
@@ -63,7 +63,7 @@ def config_app(
         config_file_path: str,
         asgi=False):
     print(f'Updating sys.path from {sys.path}... ', end='')
-    sys.path.append(os.path.abspath(path=app_dir_path)
+    sys.path.append(Path(app_dir_path).resolve())
     print(f'to {sys.path}!')
 
     import settings as _settings
