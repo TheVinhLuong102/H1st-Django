e382332c408c6e1515ab1d5968699e1a0eb2187a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-02-02 11:06:17 -0800

    add h1st/workflow(s) URLs

--- h1st/django/urls.py
+++ h1st/django/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
 
     path(route='models/',
          view=include(model_urls)),
+    path(route='workflows/',
+         view=include(model_urls)),
+    path(route='workflow/',
+         view=include(model_urls)),
 
     path(route='trust/',
          view=include(trust_urls))
